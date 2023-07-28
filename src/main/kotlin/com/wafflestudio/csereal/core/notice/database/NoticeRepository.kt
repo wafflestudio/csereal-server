cb2e22b50f8aabf4e77430765865adfebb1fd0fb
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2023-07-28 21:17:40 +0900

    feat: queryDsl 도입

--- src/main/kotlin/com/wafflestudio/csereal/core/notice/database/NoticeRepository.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/notice/database/NoticeRepository.kt
@@ -1,6 +1,22 @@
 package com.wafflestudio.csereal.core.notice.database
 
+import com.querydsl.jpa.impl.JPAQueryFactory
+import com.wafflestudio.csereal.core.notice.dto.NoticeDto
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Component
 
-interface NoticeRepository : JpaRepository<NoticeEntity, Long> {
+interface NoticeRepository : JpaRepository<NoticeEntity, Long>, CustomNoticeRepository {
+}
+
+interface CustomNoticeRepository {
+    fun searchNotice(tag: List<Long>, keyword: String) : List<NoticeDto>
+}
+@Component
+class NoticeRepositoryImpl(
+    private val queryFactory: JPAQueryFactory,
+) : CustomNoticeRepository {
+    override fun searchNotice(tag: List<Long>, keyword: String): List<NoticeDto> {
+        TODO("Not yet implemented")
+    }
+
 }
