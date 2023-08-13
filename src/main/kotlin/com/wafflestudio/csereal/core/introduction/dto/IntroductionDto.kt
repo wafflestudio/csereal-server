b9e91434708dd1eb2997e097ab1826197af15fa5
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2023-08-13 12:46:13 +0900

    fix: dto에서 postType 삭제

--- src/main/kotlin/com/wafflestudio/csereal/core/introduction/dto/IntroductionDto.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/introduction/dto/IntroductionDto.kt
@@ -6,7 +6,6 @@ import java.time.LocalDateTime
 
 data class IntroductionDto(
     val id: Long,
-    val postType: String,
     val title: String,
     val description: String,
     val year: Int?,
@@ -19,7 +18,6 @@ data class IntroductionDto(
         fun of(entity: IntroductionEntity) : IntroductionDto = entity.run {
             IntroductionDto(
                 id = this.id,
-                postType = this.postType,
                 title = this.title,
                 description = this.description,
                 year = this.year,
