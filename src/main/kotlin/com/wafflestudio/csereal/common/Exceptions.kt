be6105d5e5770f6685687e98bd4c475ac7811846
leeeryboy <indiv0227@snu.ac.kr>
2023-08-21 17:22:54 +0900

    feat: 일반 예약 및 정기 예약

--- src/main/kotlin/com/wafflestudio/csereal/common/Exceptions.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/Exceptions.kt
@@ -6,4 +6,5 @@ open class CserealException(msg: String, val status: HttpStatus) : RuntimeExcept
     class Csereal400(msg: String) : CserealException(msg, HttpStatus.BAD_REQUEST)
     class Csereal404(msg: String) : CserealException(msg, HttpStatus.NOT_FOUND)
     class Csereal401(msg: String) : CserealException(msg, HttpStatus.UNAUTHORIZED)
+    class Csereal409(msg: String) : CserealException(msg, HttpStatus.CONFLICT)
 }
