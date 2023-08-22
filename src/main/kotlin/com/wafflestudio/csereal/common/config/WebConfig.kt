28fa788f5495186cdcedd25065565414b4fd082c
Junhyeong Kim <indiv0227@snu.ac.kr>
2023-08-22 16:52:07 +0900

    fix: cors 추가 설정 (#32)
    * feat: cors 설정
    
    * feat: cors 설정

---
+++ src/main/kotlin/com/wafflestudio/csereal/common/config/WebConfig.kt
@@ -0,0 +1,18 @@
+package com.wafflestudio.csereal.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://cse-dev-waffle.bacchus.io:3000")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .maxAge(3000)
+    }
+
+}
