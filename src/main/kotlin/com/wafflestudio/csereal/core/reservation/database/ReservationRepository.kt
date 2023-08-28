db9fd9384311d2fdbb8daaadda204f8f41e7b925
leeeryboy <indiv0227@snu.ac.kr>
2023-08-28 21:32:58 +0900

    feat: 동시 예약 방지

--- src/main/kotlin/com/wafflestudio/csereal/core/reservation/database/ReservationRepository.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/reservation/database/ReservationRepository.kt
@@ -1,12 +1,15 @@
 package com.wafflestudio.csereal.core.reservation.database
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 import java.time.LocalDateTime
 import java.util.UUID
 
 interface ReservationRepository : JpaRepository<ReservationEntity, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT r FROM reservation r WHERE r.room.id = :roomId AND ((:start <= r.startTime AND r.startTime < :end) OR (:start < r.endTime AND r.endTime <= :end) OR (r.startTime <= :start AND r.endTime >= :end))")
     fun findByRoomIdAndTimeOverlap(roomId: Long, start: LocalDateTime, end: LocalDateTime): List<ReservationEntity>
 
