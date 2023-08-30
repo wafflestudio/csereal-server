23ecd9d8b2a45010687aba4d78006380c9ff344a
huGgW <whjoon0225@naver.com>
2023-08-31 01:01:34 +0900

    Refactor: Add /api/v1 in front of all endpoints (except swagger-ui)

--- src/main/kotlin/com/wafflestudio/csereal/common/controller/CommonController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/controller/CommonController.kt
@@ -3,7 +3,7 @@ package com.wafflestudio.csereal.common.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController("/api/v1")
 class CommonController {
     @GetMapping("/helloworld")
     fun helloWorld(): String {
