fb5193fa5dc71848f86ac6e1a7df7f96c9c50aa2
Junhyeong Kim <indiv0227@snu.ac.kr>
2023-08-30 22:55:33 +0900

    feat: Top Conference List GET API (#47)
    * feat: Top Conference List GET API
    
    * feat: db에서 가져올때 정렬

---
+++ src/main/kotlin/com/wafflestudio/csereal/core/conference/dto/ConferencePage.kt
@@ -0,0 +1,22 @@
+package com.wafflestudio.csereal.core.conference.dto
+
+import com.wafflestudio.csereal.core.conference.database.ConferencePageEntity
+import java.time.LocalDateTime
+
+data class ConferencePage(
+    val createdAt: LocalDateTime,
+    val modifiedAt: LocalDateTime,
+    val author: String,
+    val conferenceList: List<ConferenceDto>
+) {
+    companion object {
+        fun of(conferencePageEntity: ConferencePageEntity): ConferencePage {
+            return ConferencePage(
+                createdAt = conferencePageEntity.createdAt!!,
+                modifiedAt = conferencePageEntity.modifiedAt!!,
+                author = conferencePageEntity.author.name,
+                conferenceList = conferencePageEntity.conferences.map { ConferenceDto.of(it) }
+            )
+        }
+    }
+}
