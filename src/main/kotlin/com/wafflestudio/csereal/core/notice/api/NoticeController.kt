3b5d67a0c1b8393c4db05396203e0cfb3d2fa6d8
Junhyeong Kim <indiv0227@snu.ac.kr>
2023-08-31 09:25:23 +0900

    feat: 공지사항 글쓴이 추가 (#49)

--- src/main/kotlin/com/wafflestudio/csereal/core/notice/api/NoticeController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/notice/api/NoticeController.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.csereal.core.notice.api
 
+import com.wafflestudio.csereal.common.aop.AuthenticatedStaff
 import com.wafflestudio.csereal.core.notice.dto.*
 import com.wafflestudio.csereal.core.notice.service.NoticeService
 import jakarta.validation.Valid
@@ -14,25 +15,27 @@ class NoticeController(
 ) {
     @GetMapping
     fun searchNotice(
-        @RequestParam(required = false) tag : List<String>?,
+        @RequestParam(required = false) tag: List<String>?,
         @RequestParam(required = false) keyword: String?,
         @RequestParam(required = false, defaultValue = "0") pageNum: Long
     ): ResponseEntity<NoticeSearchResponse> {
         return ResponseEntity.ok(noticeService.searchNotice(tag, keyword, pageNum))
     }
+
     @GetMapping("/{noticeId}")
     fun readNotice(
         @PathVariable noticeId: Long,
-        @RequestParam(required = false) tag : List<String>?,
+        @RequestParam(required = false) tag: List<String>?,
         @RequestParam(required = false) keyword: String?,
-    ) : ResponseEntity<NoticeDto> {
-        return ResponseEntity.ok(noticeService.readNotice(noticeId,tag,keyword))
+    ): ResponseEntity<NoticeDto> {
+        return ResponseEntity.ok(noticeService.readNotice(noticeId, tag, keyword))
     }
 
+    @AuthenticatedStaff
     @PostMapping
     fun createNotice(
         @Valid @RequestBody request: NoticeDto
-    ) : ResponseEntity<NoticeDto> {
+    ): ResponseEntity<NoticeDto> {
         return ResponseEntity.ok(noticeService.createNotice(request))
     }
 
@@ -40,7 +43,7 @@ class NoticeController(
     fun updateNotice(
         @PathVariable noticeId: Long,
         @Valid @RequestBody request: NoticeDto,
-    ) : ResponseEntity<NoticeDto> {
+    ): ResponseEntity<NoticeDto> {
         return ResponseEntity.ok(noticeService.updateNotice(noticeId, request))
     }
 
@@ -54,7 +57,7 @@ class NoticeController(
     @PostMapping("/tag")
     fun enrollTag(
         @RequestBody tagName: Map<String, String>
-    ) : ResponseEntity<String> {
+    ): ResponseEntity<String> {
         noticeService.enrollTag(tagName["name"]!!)
         return ResponseEntity<String>("등록되었습니다. (tagName: ${tagName["name"]})", HttpStatus.OK)
     }
