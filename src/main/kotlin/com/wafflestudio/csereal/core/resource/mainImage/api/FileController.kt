4511a85330b627d77aae3e69fc0b8117037fea8d
huGgW <whjoon0225@naver.com>
2023-09-01 12:39:04 +0900

    Refactor: Change mapping of file to append /api/v1 in front.

--- src/main/kotlin/com/wafflestudio/csereal/core/resource/mainImage/api/FileController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/resource/mainImage/api/FileController.kt
@@ -14,7 +14,7 @@ import java.nio.file.Paths
 import kotlin.text.Charsets.UTF_8
 
 
-@RequestMapping("/file")
+@RequestMapping("/api/v1/file")
 @RestController
 class FileController(
     @Value("\${csereal.upload.path}")
