1cd0c7ec0ab9c715893d1c044c15bf6130b34edb
huGgW <whjoon0225@naver.com>
2023-09-08 22:24:27 +0900

    Feat: MemberSearchEntity 추가.

---
+++ src/main/kotlin/com/wafflestudio/csereal/core/member/database/MemberSearchEntity.kt
@@ -0,0 +1,101 @@
+package com.wafflestudio.csereal.core.member.database
+
+import com.wafflestudio.csereal.common.CserealException
+import com.wafflestudio.csereal.common.config.BaseTimeEntity
+import jakarta.persistence.*
+
+@Entity(name = "member_search")
+class MemberSearchEntity (
+        @Column(columnDefinition = "TEXT")
+        var content: String,
+
+        @OneToOne
+        @JoinColumn(name = "professor_id")
+        val professor: ProfessorEntity? = null,
+
+        @OneToOne
+        @JoinColumn(name = "staff_id")
+        val staff: StaffEntity? = null,
+): BaseTimeEntity() {
+    companion object {
+        fun create(professor: ProfessorEntity): MemberSearchEntity {
+            return MemberSearchEntity(
+                    content = createContent(professor),
+                    professor = professor
+            )
+        }
+
+        fun create(staff: StaffEntity): MemberSearchEntity {
+            return MemberSearchEntity(
+                    content = createContent(staff),
+                    staff = staff
+            )
+        }
+
+        fun createContent(professor: ProfessorEntity): String {
+            val stringBuilder = StringBuilder()
+            stringBuilder.appendLine(professor.name)
+            stringBuilder.appendLine(professor.status.krValue)
+            stringBuilder.appendLine(professor.academicRank)
+            professor.lab?.let { stringBuilder.appendLine(it.name) }
+            professor.startDate?.let { stringBuilder.appendLine(it) }
+            professor.endDate?.let { stringBuilder.appendLine(it) }
+            professor.office?.let { stringBuilder.appendLine(it) }
+            professor.phone?.let { stringBuilder.appendLine(it) }
+            professor.fax?.let { stringBuilder.appendLine(it) }
+            professor.email?.let { stringBuilder.appendLine(it) }
+            professor.website?.let { stringBuilder.appendLine(it) }
+            professor.educations.forEach { stringBuilder.appendLine(it.name) }
+            professor.researchAreas.forEach { stringBuilder.appendLine(it.name) }
+            professor.careers.forEach { stringBuilder.appendLine(it.name) }
+
+                    return stringBuilder.toString()
+        }
+
+        fun createContent(staff: StaffEntity): String {
+            val stringBuilder = StringBuilder()
+            stringBuilder.appendLine(staff.name)
+            stringBuilder.appendLine(staff.role)
+            stringBuilder.appendLine(staff.office)
+            stringBuilder.appendLine(staff.phone)
+            stringBuilder.appendLine(staff.email)
+            staff.tasks.forEach { stringBuilder.appendLine(it.name) }
+
+            return stringBuilder.toString()
+        }
+    }
+
+    @PrePersist
+    @PreUpdate
+    fun checkType() {
+        if (
+                (professor != null && staff != null)
+                || (professor == null && staff == null)
+        ) {
+            throw RuntimeException("MemberSearchEntity must have either professor or staff")
+        }
+    }
+
+    fun ofType(): MemberSearchType {
+        return if (professor != null) {
+            MemberSearchType.PROFESSOR
+        } else if (staff != null) {
+            MemberSearchType.STAFF
+        } else {
+            throw RuntimeException("MemberSearchEntity must have either professor or staff")
+        }
+    }
+
+    fun update(professor: ProfessorEntity) {
+        this.content = createContent(professor)
+    }
+
+    fun update(staff: StaffEntity) {
+        this.content = createContent(staff)
+    }
+}
+
+enum class MemberSearchType {
+    PROFESSOR,
+    STAFF
+}
