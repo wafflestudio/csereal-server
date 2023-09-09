09ae2c8a98314bdb2d748244e418f1415435ed41
huGgW <whjoon0225@naver.com>
2023-09-09 19:42:29 +0900

    Feat: Add controller for member search.

---
+++ src/main/kotlin/com/wafflestudio/csereal/core/member/api/MemberSearchController.kt
@@ -0,0 +1,27 @@
+package com.wafflestudio.csereal.core.member.api
+
+import com.wafflestudio.csereal.core.member.service.MemberSearchService
+import org.springframework.data.jpa.repository.Query
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/member/search")
+class MemberSearchController(
+        private val memberSearchService: MemberSearchService,
+) {
+    @GetMapping("/top")
+    fun searchTop(
+            @RequestParam(required = true) keyword: String,
+            @RequestParam(required = true) number: Int,
+    ) = memberSearchService.searchTopMember(keyword, number)
+
+    @GetMapping
+    fun searchPage(
+            @RequestParam(required = true) keyword: String,
+            @RequestParam(required = true) pageSize: Int,
+            @RequestParam(required = true) pageNum: Int,
+    )= memberSearchService.searchMember(keyword, pageSize, pageNum)
+}
