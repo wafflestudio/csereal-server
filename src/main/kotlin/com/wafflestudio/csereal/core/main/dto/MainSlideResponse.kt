6f726899a66c316cdf4930172420d34c21f8a373
Jo Seonggyu <blueland1998@snu.ac.kr>
2023-09-10 17:06:19 +0900

    fix: main 프론트에 맞게 협의, notice 태그 enum 추가 (#83)
    * fix: mainResponse 다시 작성
    
    * feat: TagInNoticeEnum 추가
    
    * feat: notice enum 추가
    
    * fix: 코드 수정

--- src/main/kotlin/com/wafflestudio/csereal/core/main/dto/MainSlideResponse.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/main/dto/MainSlideResponse.kt
@@ -3,9 +3,10 @@ package com.wafflestudio.csereal.core.main.dto
 import com.querydsl.core.annotations.QueryProjection
 import java.time.LocalDateTime
 
-data class NewsResponse @QueryProjection constructor(
+data class MainSlideResponse @QueryProjection constructor(
     val id: Long,
     val title: String,
+    val imageURL: String?,
     val createdAt: LocalDateTime?
 ) {
 
---
+++ src/main/kotlin/com/wafflestudio/csereal/core/notice/database/TagInNoticeEnum.kt
@@ -0,0 +1,33 @@
+package com.wafflestudio.csereal.core.notice.database
+
+import com.wafflestudio.csereal.common.CserealException
+
+enum class TagInNoticeEnum {
+    CLASS, SCHOLARSHIP, UNDERGRADUATE, GRADUATE, MINOR, REGISTRATIONS, ADMISSIONS, GRADUATIONS,
+    RECRUIT, STUDENT_EXCHANGE, EVENTS_PROGRAMS, FOREIGN;
+
+    companion object {
+        fun getTagEnum(t: String) : TagInNoticeEnum{
+            return when(t) {
+                "수업" -> CLASS
+                "장학" -> SCHOLARSHIP
+                "학사(학부)" -> UNDERGRADUATE
+                "학사(대학원)" -> GRADUATE
+                "다전공/전과" -> MINOR
+                "등록/복학/휴학/재입학" -> REGISTRATIONS
+                "입학" -> ADMISSIONS
+                "졸업" -> GRADUATIONS
+                "채용정보" -> RECRUIT
+                "교환학생/유학" -> STUDENT_EXCHANGE
+                "행사/프로그램" -> EVENTS_PROGRAMS
+                "foreign" -> FOREIGN
+                else -> throw CserealException.Csereal404("태그를 찾을 수 없습니다")
+            }
+        }
+    }
+
+
+}
+
+
+
