c06e73b19170c1fb5c7343e575aa72968dbe278b
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2023-09-11 14:17:03 +0900

    Feat: 구성원 검색 API 추가 (#85)
    * Feat: Add mysql custom dialect with MATCH AGAINST BOOLEAN MODE function.
    
    * CICD: Add config for mysql db
    
    * CICD: Add volumes for config file.
    
    * CICD: Add db_config for condition for db deploy
    
    * Feat: Add search top, and search query.
    
    * Feat: Add DTOs
    
    * Feat: Add Controller for member search only top, and page.
    
    * Feat: Add dialect config for application.yaml
    
    * Feat: Add controller for member search.
    
    * Refactor: Change exception message to korean.
    
    ---------
    
    Co-authored-by: Jo Seonggyu <blueland1998@snu.ac.kr>

--- src/main/kotlin/com/wafflestudio/csereal/core/member/database/MemberSearchRepository.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/member/database/MemberSearchRepository.kt
@@ -1,6 +1,11 @@
 package com.wafflestudio.csereal.core.member.database
 
+import com.querydsl.core.types.dsl.Expressions
+import com.querydsl.jpa.impl.JPAQuery
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.wafflestudio.csereal.core.member.database.QMemberSearchEntity.memberSearchEntity
+import com.wafflestudio.csereal.core.member.database.QProfessorEntity.professorEntity
+import com.wafflestudio.csereal.core.member.database.QStaffEntity.staffEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
@@ -9,11 +14,78 @@ interface MemberSearchRepository
 }
 
 interface MemberSearchRepositoryCustom {
-
+    fun searchTopMember(keyword: String, number: Int): List<MemberSearchEntity>
+    fun searchMember(keyword: String, pageSize: Int, pageNum: Int): Pair<List<MemberSearchEntity>, Long>
 }
 
 @Repository
 class MemberSearchRepositoryCustomImpl (
         private val queryFactory: JPAQueryFactory,
 ): MemberSearchRepositoryCustom {
+
+    override fun searchTopMember(keyword: String, number: Int): List<MemberSearchEntity> {
+        return searchQuery(keyword)
+                .limit(number.toLong())
+                .fetch()
+    }
+
+    override fun searchMember(keyword: String, pageSize: Int, pageNum: Int): Pair<List<MemberSearchEntity>, Long> {
+        val query = searchQuery(keyword)
+        val total = getSearchCount(keyword)
+
+        val validPageNum = exchangePageNum(pageSize, pageNum, total)
+        val queryResult = query
+                .offset((validPageNum-1) * pageSize.toLong())
+                .limit(pageSize.toLong())
+                .fetch()
+
+        return queryResult to total
+    }
+
+    fun searchFullTextTemplate(keyword: String) =
+        Expressions.numberTemplate(
+                Double::class.javaObjectType,
+                "function('match',{0},{1})",
+                memberSearchEntity.content,
+                keyword
+        )
+
+    fun searchQuery(keyword: String): JPAQuery<MemberSearchEntity> {
+        val searchDoublTemplate = searchFullTextTemplate(keyword)
+
+        return queryFactory.select(
+                memberSearchEntity
+        ).from(
+                memberSearchEntity
+        ).leftJoin(
+                memberSearchEntity.professor, professorEntity
+        ).fetchJoin()
+                .leftJoin(
+                        memberSearchEntity.staff, staffEntity
+                ).fetchJoin()
+                .where(
+                        searchDoublTemplate.gt(0.0)
+                )
+    }
+
+    fun getSearchCount(keyword: String): Long {
+        val searchDoubleTemplate = searchFullTextTemplate(keyword)
+
+        return queryFactory.select(
+                memberSearchEntity
+                    .countDistinct()
+        ).from(
+                memberSearchEntity
+        ).where(
+                    searchDoubleTemplate.gt(0.0)
+        ).fetchOne()!!
+    }
+
+    fun exchangePageNum(pageSize: Int, pageNum: Int, total: Long): Int {
+        return if ((pageNum - 1) * pageSize < total) {
+            pageNum
+        } else {
+            Math.ceil(total.toDouble() / pageSize).toInt()
+        }
+    }
 }
---
+++ src/main/kotlin/com/wafflestudio/csereal/core/member/dto/MemberSearchPageResponse.kt
@@ -0,0 +1,20 @@
+package com.wafflestudio.csereal.core.member.dto
+
+import com.wafflestudio.csereal.core.member.database.MemberSearchEntity
+import com.wafflestudio.csereal.core.resource.mainImage.database.MainImageEntity
+
+data class MemberSearchPageResponse(
+        val members: List<MemberSearchResponseElement>,
+        val total: Long,
+) {
+    companion object {
+        fun of(
+                members: List<MemberSearchEntity>,
+                total: Long,
+                imageURLMaker: (MainImageEntity?) -> String?
+        ) = MemberSearchPageResponse(
+                    members = members.map { MemberSearchResponseElement.of(it, imageURLMaker) },
+                    total = total,
+                )
+    }
+}
