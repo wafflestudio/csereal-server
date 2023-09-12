972bafecb8f628e71cc4886db664ad5532fb4000
huGgW <whjoon0225@naver.com>
2023-09-12 16:00:52 +0900

    Feat: Extract full text search template to common repository.

---
+++ src/main/kotlin/com/wafflestudio/csereal/common/repository/CommonRepository.kt
@@ -0,0 +1,50 @@
+package com.wafflestudio.csereal.common.repository
+
+import com.querydsl.core.types.dsl.Expressions
+import com.querydsl.core.types.dsl.NumberTemplate
+import org.springframework.stereotype.Repository
+
+interface CommonRepository {
+    fun searchFullSingleTextTemplate(keyword: String, field: Any): NumberTemplate<Double>
+    fun searchFullDoubleTextTemplate(keyword: String, field1: Any, field2: Any): NumberTemplate<Double>
+    fun searchFullTripleTextTemplate(keyword: String, field1: Any, field2: Any, field3: Any): NumberTemplate<Double>
+}
+
+@Repository
+class CommonRepositoryImpl: CommonRepository {
+    override fun searchFullSingleTextTemplate(
+            keyword: String,
+            field: Any,
+    ) = Expressions.numberTemplate(
+            Double::class.javaObjectType,
+            "function('match',{0},{1})",
+            field,
+            keyword
+        )
+
+    override fun searchFullDoubleTextTemplate(
+            keyword: String,
+            field1: Any,
+            field2: Any,
+    ) = Expressions.numberTemplate(
+            Double::class.javaObjectType,
+            "function('match2',{0},{1},{2})",
+            field1,
+            field2,
+            keyword,
+    )
+
+    override fun searchFullTripleTextTemplate(
+            keyword: String,
+            field1: Any,
+            field2: Any,
+            field3: Any,
+    ) = Expressions.numberTemplate(
+            Double::class.javaObjectType,
+            "function('match3',{0},{1},{2},{3})",
+            field1,
+            field2,
+            field3,
+            keyword
+    )
+}
