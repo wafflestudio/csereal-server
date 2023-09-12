d5d3b7c9ffe32c6a44aff6afacdc9cea3e5e365f
Junhyeong Kim <indiv0227@snu.ac.kr>
2023-09-12 18:08:05 +0900

    fix: GET notice 권한 관련 로직 수정 (#101)

--- src/main/kotlin/com/wafflestudio/csereal/core/notice/api/NoticeController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/notice/api/NoticeController.kt
@@ -3,10 +3,14 @@ package com.wafflestudio.csereal.core.notice.api
 import com.wafflestudio.csereal.common.aop.AuthenticatedStaff
 import com.wafflestudio.csereal.core.notice.dto.*
 import com.wafflestudio.csereal.core.notice.service.NoticeService
+import com.wafflestudio.csereal.core.user.database.Role
+import com.wafflestudio.csereal.core.user.database.UserRepository
 import jakarta.validation.Valid
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
@@ -14,17 +18,27 @@ import org.springframework.web.multipart.MultipartFile
 @RestController
 class NoticeController(
     private val noticeService: NoticeService,
+    private val userRepository: UserRepository
 ) {
     @GetMapping
     fun searchNotice(
         @RequestParam(required = false) tag: List<String>?,
         @RequestParam(required = false) keyword: String?,
-        @RequestParam(required = false, defaultValue = "1") pageNum: Int
+        @RequestParam(required = false, defaultValue = "1") pageNum: Int,
+        @AuthenticationPrincipal oidcUser: OidcUser?
     ): ResponseEntity<NoticeSearchResponse> {
+        var isStaff = false
+        if (oidcUser != null) {
+            val username = oidcUser.idToken.getClaim<String>("username")
+            val user = userRepository.findByUsername(username)
+            if (user?.role == Role.ROLE_STAFF) {
+                isStaff = true
+            }
+        }
         val pageSize = 20
         val pageRequest = PageRequest.of(pageNum - 1, pageSize)
         val usePageBtn = pageNum != 1
-        return ResponseEntity.ok(noticeService.searchNotice(tag, keyword, pageRequest, usePageBtn))
+        return ResponseEntity.ok(noticeService.searchNotice(tag, keyword, pageRequest, usePageBtn, isStaff))
     }
 
     @GetMapping("/{noticeId}")
