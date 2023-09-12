ba8aace4050c763d8dd688d34f7ba553ef3452e6
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2023-09-12 14:19:30 +0900

    Feat: Conference page 수정 API (#99)
    * Feat: Add Controller for member search only top, and page.
    
    * Feat: Add ResearchSearchEntity, and entry for conference, research, lab entity.
    
    * Feat: For research create/update, lab create, add updating researchsearch.
    
    * Feat: Add empty repository and service for ResearchSearch
    
    * Feat: Add update method for labentity.
    
    * Feat: Add update lab service.
    
    * Feat: Add update lab controller.
    
    * Test: Add ResearchServiceTest.
    
    * Feat: Add Conference Repository.
    
    * Feat: add conferenc eentity method for create, update entity.
    
    * Feat: Change variables to mutable.
    
    * Feat: Add id in conferencedto.
    
    * Feat: Add to sort the conferenceList.
    
    * Feat: Add deleteAttachment (marking)
    
    * Feat add service to modify conference page (create, modify, delete conferences)
    
    * Feat: Add api for modify conference page.
    
    * Test: Add test for conference service.
    
    ---------
    
    Co-authored-by: Junhyeong Kim <indiv0227@snu.ac.kr>

---
+++ src/test/kotlin/com/wafflestudio/csereal/core/conference/service/ConferenceServiceTest.kt
@@ -0,0 +1,160 @@
+package com.wafflestudio.csereal.core.conference.service
+
+import com.wafflestudio.csereal.core.conference.database.ConferenceEntity
+import com.wafflestudio.csereal.core.conference.database.ConferencePageEntity
+import com.wafflestudio.csereal.core.conference.database.ConferencePageRepository
+import com.wafflestudio.csereal.core.conference.database.ConferenceRepository
+import com.wafflestudio.csereal.core.conference.dto.ConferenceCreateDto
+import com.wafflestudio.csereal.core.conference.dto.ConferenceDto
+import com.wafflestudio.csereal.core.conference.dto.ConferenceModifyRequest
+import com.wafflestudio.csereal.core.user.database.Role
+import com.wafflestudio.csereal.core.user.database.UserEntity
+import com.wafflestudio.csereal.core.user.database.UserRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringTestExtension
+import io.kotest.extensions.spring.SpringTestLifecycleMode
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.RequestContextHolder
+
+@SpringBootTest
+@Transactional
+class ConferenceServiceTest (
+        private val conferenceService: ConferenceService,
+        private val conferencePageRepository: ConferencePageRepository,
+        private val conferenceRepository: ConferenceRepository,
+        private val userRepository: UserRepository,
+): BehaviorSpec ({
+    extensions(SpringTestExtension(SpringTestLifecycleMode.Root))
+
+    beforeSpec {
+        val user = userRepository.save(
+                UserEntity(
+                        username = "admin",
+                        name = "admin",
+                        email = "email",
+                        studentId = "studentId",
+                        role = Role.ROLE_STAFF,
+                )
+        )
+
+        conferencePageRepository.save(
+                ConferencePageEntity(
+                        author = user,
+                )
+        )
+    }
+
+    afterSpec {
+        conferencePageRepository.deleteAll()
+        conferenceRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+
+    // ConferencePage
+    Given("Conference를 수정하려고 할 때") {
+        val userEntity = userRepository.findByUsername("admin")!!
+
+        mockkStatic(RequestContextHolder::class)
+        val mockRequestAttributes = mockk<RequestAttributes>()
+        every {
+            RequestContextHolder.getRequestAttributes()
+        } returns mockRequestAttributes
+        every {
+            mockRequestAttributes.getAttribute(
+                    "loggedInUser",
+                    RequestAttributes.SCOPE_REQUEST
+            )
+        } returns userEntity
+
+
+        var conferencePage = conferencePageRepository.findAll().first()
+
+        val conferences = conferenceRepository.saveAll(listOf(
+                ConferenceEntity(
+                        code = "code1",
+                        name = "name1",
+                        abbreviation = "abbreviation1",
+                        conferencePage = conferencePage,
+                ),
+                ConferenceEntity(
+                        code = "code2",
+                        name = "name2",
+                        abbreviation = "abbreviation2",
+                        conferencePage = conferencePage,
+                ),
+                ConferenceEntity(
+                        code = "code3",
+                        name = "name3",
+                        abbreviation = "abbreviation3",
+                        conferencePage = conferencePage,
+                ),
+        ))
+        conferencePage = conferencePage.apply {
+            this.conferences.addAll(conferences)
+        }.let {
+            conferencePageRepository.save(it)
+        }
+
+        When("Conference를 수정한다면") {
+            val deleteConferenceId = conferences[1].id
+            val modifiedConference = ConferenceDto(
+                            id = conferences.first().id,
+                            code = "code0",
+                            name = "modifiedName",
+                            abbreviation = "modifiedAbbreviation",
+                    )
+            val newConference = ConferenceCreateDto(
+                            code = "code9",
+                            name = "newName",
+                            abbreviation = "newAbbreviation",
+                    )
+            val conferenceModifyRequest = ConferenceModifyRequest(
+                    deleteConfereceIdList = listOf(deleteConferenceId),
+                    modifiedConferenceList = listOf(modifiedConference),
+                    newConferenceList = listOf(newConference),
+            )
+
+            val conferencePage = conferenceService.modifyConferences(conferenceModifyRequest)
+
+            Then("Conference가 수정되어야 한다.") {
+                val newConferencePage = conferencePageRepository.findAll().first()
+                val newConferences = newConferencePage.conferences.sortedBy { it.code }
+
+                newConferences.size shouldBe 3
+                newConferences.first().apply {
+                    code shouldBe modifiedConference.code
+                    name shouldBe modifiedConference.name
+                    abbreviation shouldBe modifiedConference.abbreviation
+                    researchSearch?.content shouldBe """
+                        modifiedName
+                        code0
+                        modifiedAbbreviation
+                        
+                    """.trimIndent()
+                }
+                newConferences[1].apply {
+                    code shouldBe conferences.last().code
+                    name shouldBe conferences.last().name
+                    abbreviation shouldBe conferences.last().abbreviation
+                }
+                newConferences.last().apply {
+                    code shouldBe newConference.code
+                    name shouldBe newConference.name
+                    abbreviation shouldBe newConference.abbreviation
+                    researchSearch?.content shouldBe """
+                        newName
+                        code9
+                        newAbbreviation
+                        
+                    """.trimIndent()
+                }
+            }
+        }
+    }
+})
