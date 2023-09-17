e744b210f3b254155fca93ce899a1d9b4c3bf1cd
Jo Seonggyu <blueland1998@snu.ac.kr>
2023-09-17 09:15:04 +0900

    fix: news, notice에 titleForMain 추가 (#125)
    * fix: 소식3총사 titleForMain 데이터 생성
    
    * fix: 테스트에 titleForMain  추가
    
    * Revert "fix: 테스트에 titleForMain  추가"
    
    This reverts commit e824ad34e60e0785f1d4c4e187776d4b8bf53dfa.
    
    * fix: 테스트에 titleForMain 추가
    
    * fix: seminar titleForMain 추가
    
    * fix: seminar test titleForMain 추가

--- src/test/kotlin/com/wafflestudio/csereal/core/seminar/service/SeminarServiceTest.kt
+++ src/test/kotlin/com/wafflestudio/csereal/core/seminar/service/SeminarServiceTest.kt
@@ -30,6 +30,7 @@ class SeminarServiceTest(
             val seminarDTO = SeminarDto(
                 id = -1,
                 title = "title",
+                titleForMain = null,
                 description = """
                         <h1>Hello, World!</h1>
                         <p>This is seminar description.</p>
@@ -86,6 +87,7 @@ class SeminarServiceTest(
             val originalSeminar = seminarRepository.save(
                 SeminarEntity(
                     title = "title",
+                    titleForMain = null,
                     description = """
                                 <h1>Hello, World!</h1>
                                 <p>This is seminar description.</p>
