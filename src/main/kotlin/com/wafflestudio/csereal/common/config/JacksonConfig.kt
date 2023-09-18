889b499d951b6ad18138ee4f877c0512e63438d4
leeeryboy <indiv0227@snu.ac.kr>
2023-09-18 14:59:05 +0900

    fix: 기존 objectMapper 설정 오버라이드

--- src/main/kotlin/com/wafflestudio/csereal/common/config/JacksonConfig.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/config/JacksonConfig.kt
@@ -5,22 +5,22 @@ import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.module.SimpleModule
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.EventListener
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 @Configuration
-class JacksonConfig {
+class JacksonConfig(private val objectMapper: ObjectMapper) {
 
-    @Bean
-    fun objectMapper(): ObjectMapper {
-        val objectMapper = ObjectMapper()
+    @EventListener(ApplicationReadyEvent::class)
+    fun setUp() {
         val module = SimpleModule()
         module.addSerializer(LocalDateTime::class.java, LocalDateTimeSerializer())
         objectMapper.registerModule(module)
-        return objectMapper
     }
 }
 
