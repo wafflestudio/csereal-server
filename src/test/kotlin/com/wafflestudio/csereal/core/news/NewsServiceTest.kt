5506f1c332f58c7f853d1f73e7c2145aa8088e05
huGgW <whjoon0225@naver.com>
2023-09-20 19:54:35 +0900

    Test: Add test for read all slides.

--- src/test/kotlin/com/wafflestudio/csereal/core/news/NewsServiceTest.kt
+++ src/test/kotlin/com/wafflestudio/csereal/core/news/NewsServiceTest.kt
@@ -5,6 +5,8 @@ import com.wafflestudio.csereal.core.news.database.NewsRepository
 import com.wafflestudio.csereal.core.news.dto.NewsDto
 import com.wafflestudio.csereal.core.news.service.NewsService
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringTestExtension
+import io.kotest.extensions.spring.SpringTestLifecycleMode
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import org.springframework.boot.test.context.SpringBootTest
@@ -17,6 +19,7 @@ class NewsServiceTest(
     private val newsRepository: NewsRepository
 ) : BehaviorSpec() {
     init {
+        extensions(SpringTestExtension(SpringTestLifecycleMode.Root))
 
         afterSpec {
             newsRepository.deleteAll()
@@ -110,6 +113,89 @@ class NewsServiceTest(
                         "Hello, World! This is modified news description." +
                             " Goodbye, World! This is additional description."
                         )
+                }
+            }
+        }
+
+        Given("Slide된 뉴스 2개, Slide되지 않은 뉴스가 2개 있을 때") {
+            val newsEntitySlide1 = newsRepository.save(
+                NewsEntity(
+                    title = "title",
+                    titleForMain = null,
+                    description = """
+                            <h1>Hello, World!</h1>
+                            <p>This is news description.</p>
+                            <h3>Goodbye, World!</h3>
+                    """.trimIndent(),
+                    plainTextDescription = "Hello, World! This is news description. Goodbye, World!",
+                    date = LocalDateTime.now(),
+                    isPrivate = false,
+                    isSlide = true,
+                    isImportant = false
+                )
+            )
+
+            val newsEntitySlide2 = newsRepository.save(
+                NewsEntity(
+                    title = "title",
+                    titleForMain = null,
+                    description = """
+                            <h1>Hello, World!</h1>
+                            <p>This is news description.</p>
+                            <h3>Goodbye, World!</h3>
+                    """.trimIndent(),
+                    plainTextDescription = "Hello, World! This is news description. Goodbye, World!",
+                    date = LocalDateTime.now(),
+                    isPrivate = false,
+                    isSlide = true,
+                    isImportant = false
+                )
+            )
+
+            val newsEntityNoSlide1 = newsRepository.save(
+                NewsEntity(
+                    title = "title",
+                    titleForMain = null,
+                    description = """
+                            <h1>Hello, World!</h1>
+                            <p>This is news description.</p>
+                            <h3>Goodbye, World!</h3>
+                    """.trimIndent(),
+                    plainTextDescription = "Hello, World! This is news description. Goodbye, World!",
+                    date = LocalDateTime.now(),
+                    isPrivate = false,
+                    isSlide = false,
+                    isImportant = false
+                )
+            )
+
+            val newsEntityNoSlide2 = newsRepository.save(
+                NewsEntity(
+                    title = "title",
+                    titleForMain = null,
+                    description = """
+                            <h1>Hello, World!</h1>
+                            <p>This is news description.</p>
+                            <h3>Goodbye, World!</h3>
+                    """.trimIndent(),
+                    plainTextDescription = "Hello, World! This is news description. Goodbye, World!",
+                    date = LocalDateTime.now(),
+                    isPrivate = false,
+                    isSlide = false,
+                    isImportant = false
+                )
+            )
+
+            When("Slide한 뉴스 2페이지를 가져오면") {
+                val response = newsService.readAllSlides(1, 1)
+
+                Then("Slide된 뉴스 2개 중 먼저 생성된 1개가 나와야 한다.") {
+                    response.slides.size shouldBe 1
+                    response.slides.first().id shouldBe newsEntitySlide1.id
+                }
+
+                Then("총 개수가 2개가 나와야 한다.") {
+                    response.total shouldBe 2
                 }
             }
         }
