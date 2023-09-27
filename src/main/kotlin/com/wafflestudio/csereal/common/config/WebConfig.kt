cd06eec8eeebf565030d796196c54a9bd831c208
leeeryboy <indiv0227@snu.ac.kr>
2023-09-27 22:16:09 +0900

    refactor: 로그인 페이지 통합 및 CORS 설정 변경

--- src/main/kotlin/com/wafflestudio/csereal/common/config/WebConfig.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/config/WebConfig.kt
@@ -1,17 +1,23 @@
 package com.wafflestudio.csereal.common.config
 
+import com.wafflestudio.csereal.common.properties.EndpointProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+@EnableConfigurationProperties(EndpointProperties::class)
+class WebConfig(
+    private val endpointProperties: EndpointProperties
+) : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins(endpointProperties.frontend, "http://localhost:3000")
             .allowedMethods("*")
             .allowedHeaders("*")
+            .allowCredentials(true)
             .maxAge(3000)
     }
 }
