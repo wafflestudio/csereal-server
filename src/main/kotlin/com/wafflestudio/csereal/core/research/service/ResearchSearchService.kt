634f0e989d2b97acd8a0ab992054e1438d6fc7b4
huGgW <whjoon0225@naver.com>
2024-01-11 22:51:10 +0900

    Feat: Update research search when listen professor event.

--- src/main/kotlin/com/wafflestudio/csereal/core/research/service/ResearchSearchService.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/research/service/ResearchSearchService.kt
@@ -1,18 +1,70 @@
 package com.wafflestudio.csereal.core.research.service
 
+import com.wafflestudio.csereal.core.member.event.ProfessorCreatedEvent
+import com.wafflestudio.csereal.core.member.event.ProfessorDeletedEvent
+import com.wafflestudio.csereal.core.member.event.ProfessorModifiedEvent
+import com.wafflestudio.csereal.core.research.database.LabRepository
 import com.wafflestudio.csereal.core.research.database.ResearchSearchEntity
 import com.wafflestudio.csereal.core.research.database.ResearchSearchRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 interface ResearchSearchService {
+    fun professorCreatedEventListener(professorCreatedEvent: ProfessorCreatedEvent)
+    fun professorDeletedEventListener(professorDeletedEvent: ProfessorDeletedEvent)
+    fun professorModifiedEventListener(professorModifiedEvent: ProfessorModifiedEvent)
     fun deleteResearchSearch(researchSearchEntity: ResearchSearchEntity)
 }
 
 @Service
 class ResearchSearchServiceImpl(
+    private val labRepository: LabRepository,
     private val researchSearchRepository: ResearchSearchRepository
 ) : ResearchSearchService {
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional
+    override fun professorCreatedEventListener(professorCreatedEvent: ProfessorCreatedEvent) {
+        val lab = professorCreatedEvent.labId?.let {
+            labRepository.findByIdOrNull(it)
+        } ?: return
+
+        lab.researchSearch?.update(lab) ?: let {
+            lab.researchSearch = ResearchSearchEntity.create(lab)
+        }
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional
+    override fun professorDeletedEventListener(professorDeletedEvent: ProfessorDeletedEvent) {
+        val lab = professorDeletedEvent.labId?.let {
+            labRepository.findByIdOrNull(it)
+        } ?: return
+
+        lab.researchSearch?.update(lab)
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional
+    override fun professorModifiedEventListener(professorModifiedEvent: ProfessorModifiedEvent) {
+        val beforeLab = professorModifiedEvent.beforeLabId?.let {
+            labRepository.findByIdOrNull(it)
+        }
+
+        val afterLab = professorModifiedEvent.afterLabId?.let {
+            labRepository.findByIdOrNull(it)
+        }
+
+        beforeLab?.run {
+            researchSearch?.update(this)
+        }
+
+        afterLab?.run {
+            researchSearch?.update(this)
+        }
+    }
 
     @Transactional
     override fun deleteResearchSearch(
