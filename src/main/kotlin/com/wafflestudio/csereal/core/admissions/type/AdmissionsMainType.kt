725e082093914f30a4d68d75e82e40ee7a879b7d
huGgW <whjoon0225@naver.com>
2024-02-11 17:58:09 +0900

    Feat: Add korean, english section name.

--- src/main/kotlin/com/wafflestudio/csereal/core/admissions/type/AdmissionsMainType.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/admissions/type/AdmissionsMainType.kt
@@ -1,11 +1,20 @@
 package com.wafflestudio.csereal.core.admissions.type
 
 import com.wafflestudio.csereal.common.CserealException
+import com.wafflestudio.csereal.common.properties.LanguageType
 
-enum class AdmissionsMainType {
-    UNDERGRADUATE,
-    GRADUATE,
-    INTERNATIONAL;
+enum class AdmissionsMainType(
+    val ko: String,
+    val en: String
+) {
+    UNDERGRADUATE("학부", "Undergraduate"),
+    GRADUATE("대학원", "Graduate"),
+    INTERNATIONAL("International", "International");
+
+    fun getLanguageValue(language: LanguageType) = when (language) {
+        LanguageType.KO -> this.ko
+        LanguageType.EN -> this.en
+    }
 
     fun toJsonValue() = this.name.lowercase()
 
