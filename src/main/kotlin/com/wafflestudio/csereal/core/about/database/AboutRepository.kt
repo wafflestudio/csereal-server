a3e712f895ef5f70f8f6dee5cae76d45c9db7707
huGgW <whjoon0225@naver.com>
2024-02-28 01:20:19 +0900

    Feat: Add search query method.

--- src/main/kotlin/com/wafflestudio/csereal/core/about/database/AboutRepository.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/about/database/AboutRepository.kt
@@ -1,9 +1,15 @@
 package com.wafflestudio.csereal.core.about.database
 
+import com.querydsl.jpa.impl.JPAQuery
+import com.querydsl.jpa.impl.JPAQueryFactory
 import com.wafflestudio.csereal.common.properties.LanguageType
+import com.wafflestudio.csereal.common.repository.CommonRepository
+import com.wafflestudio.csereal.common.utils.exchangePageNum
+import com.wafflestudio.csereal.core.about.database.QAboutEntity.aboutEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface AboutRepository : JpaRepository<AboutEntity, Long> {
+interface AboutRepository : JpaRepository<AboutEntity, Long>, AboutCustomRepository {
     fun findAllByLanguageAndPostTypeOrderByName(
         languageType: LanguageType,
         postType: AboutPostType
@@ -12,4 +18,59 @@ interface AboutRepository : JpaRepository<AboutEntity, Long> {
         languageType: LanguageType,
         postType: AboutPostType
     ): AboutEntity
+}
+
+interface AboutCustomRepository {
+    fun searchAbouts(
+        keyword: String,
+        language: LanguageType,
+        pageSize: Int,
+        pageNum: Int
+    ): Pair<List<AboutEntity>, Long>
+}
+
+@Repository
+class AboutCustomRepositoryImpl(
+    private val queryFactory: JPAQueryFactory,
+    private val commonRepository: CommonRepository
+) : AboutCustomRepository {
+    override fun searchAbouts(
+        keyword: String,
+        language: LanguageType,
+        pageSize: Int,
+        pageNum: Int
+    ): Pair<List<AboutEntity>, Long> {
+        val total = searchCount(keyword, language)
+        val validPageNum = exchangePageNum(pageSize, pageNum, total)
+        val validOffset = (
+            if (validPageNum >= 1) validPageNum - 1 else 0
+            ) * pageSize.toLong()
+
+        val queryResult = searchQueryExpr(keyword, language)
+            .offset(validOffset)
+            .limit(pageSize.toLong())
+            .fetch()
+
+        return queryResult to total
+    }
+
+    fun searchCount(keyword: String, language: LanguageType): Long {
+        return searchQueryExpr(keyword, language)
+            .select(aboutEntity.countDistinct())
+            .fetchOne()!!
+    }
+
+    fun searchQueryExpr(keyword: String, language: LanguageType): JPAQuery<AboutEntity> {
+        val matchExpression = commonRepository.searchFullSingleTextTemplate(
+            keyword,
+            aboutEntity.searchContent
+        )
+
+        return queryFactory.select(aboutEntity)
+            .from(aboutEntity)
+            .where(
+                matchExpression.gt(0.0),
+                aboutEntity.language.eq(language)
+            )
+    }
 }
