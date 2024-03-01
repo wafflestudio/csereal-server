bffad2ee0f1fe5657d2302f342886c28632dc740
huGgW <whjoon0225@naver.com>
2024-03-02 00:06:52 +0900

    Refactor: Add language, amount query param.

--- src/main/kotlin/com/wafflestudio/csereal/core/academics/api/AcademicsController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/academics/api/AcademicsController.kt
@@ -1,11 +1,13 @@
 package com.wafflestudio.csereal.core.academics.api
 
 import com.wafflestudio.csereal.common.aop.AuthenticatedStaff
+import com.wafflestudio.csereal.common.properties.LanguageType
 import com.wafflestudio.csereal.core.academics.dto.*
 import com.wafflestudio.csereal.core.academics.service.AcademicsService
 import com.wafflestudio.csereal.core.academics.dto.ScholarshipDto
 import com.wafflestudio.csereal.core.academics.service.AcademicsSearchService
 import jakarta.validation.Valid
+import jakarta.validation.constraints.Positive
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -145,20 +147,28 @@ class AcademicsController(
     @GetMapping("/search/top")
     fun searchTop(
         @RequestParam(required = true) keyword: String,
-        @RequestParam(required = true) number: Int
+        @RequestParam(required = true) @Valid @Positive number: Int,
+        @RequestParam(required = true, defaultValue = "ko") language: String,
+        @RequestParam(required = false, defaultValue = "30") amount: Int
     ) = academicsSearchService.searchTopAcademics(
         keyword = keyword,
-        number = number
+        language = LanguageType.makeStringToLanguageType(language),
+        number = number,
+        amount = amount
     )
 
     @GetMapping("/search")
     fun searchAcademics(
         @RequestParam(required = true) keyword: String,
-        @RequestParam(required = true) pageSize: Int,
-        @RequestParam(required = true) pageNum: Int
+        @RequestParam(required = true) @Valid @Positive pageSize: Int,
+        @RequestParam(required = true) @Valid @Positive pageNum: Int,
+        @RequestParam(required = true, defaultValue = "ko") language: String,
+        @RequestParam(required = false, defaultValue = "30") amount: Int
     ) = academicsSearchService.searchAcademics(
         keyword = keyword,
+        language = LanguageType.makeStringToLanguageType(language),
         pageSize = pageSize,
-        pageNum = pageNum
+        pageNum = pageNum,
+        amount = amount
     )
 }
