1f6eb6b8eb9da69b061bab14a504ee8b5a01633e
huGgW <whjoon0225@naver.com>
2024-03-01 13:35:17 +0900

    Feat: Add language query parameter, change response body.

--- src/main/kotlin/com/wafflestudio/csereal/core/member/api/MemberSearchController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/member/api/MemberSearchController.kt
@@ -1,6 +1,9 @@
 package com.wafflestudio.csereal.core.member.api
 
+import com.wafflestudio.csereal.common.properties.LanguageType
 import com.wafflestudio.csereal.core.member.service.MemberSearchService
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Positive
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,13 +17,19 @@ class MemberSearchController(
     @GetMapping("/top")
     fun searchTop(
         @RequestParam(required = true) keyword: String,
-        @RequestParam(required = true) number: Int
-    ) = memberSearchService.searchTopMember(keyword, number)
+        @RequestParam(required = true) @Valid @Positive number: Int,
+        @RequestParam(required = true, defaultValue = "ko") language: String
+    ) = LanguageType.makeStringToLanguageType(language).let {
+        memberSearchService.searchTopMember(keyword, it, number)
+    }
 
     @GetMapping
     fun searchPage(
         @RequestParam(required = true) keyword: String,
-        @RequestParam(required = true) pageSize: Int,
-        @RequestParam(required = true) pageNum: Int
-    ) = memberSearchService.searchMember(keyword, pageSize, pageNum)
+        @RequestParam(required = true, defaultValue = "ko") language: String,
+        @RequestParam(required = true) @Valid @Positive pageSize: Int,
+        @RequestParam(required = true) @Valid @Positive pageNum: Int
+    ) = LanguageType.makeStringToLanguageType(language).let {
+        memberSearchService.searchMember(keyword, it, pageSize, pageNum)
+    }
 }
