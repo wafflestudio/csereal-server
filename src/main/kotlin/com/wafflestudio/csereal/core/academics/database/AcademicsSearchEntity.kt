597557091b47d9e5aa3f261665452f327ccb6b41
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2024-03-01 23:00:54 +0900

    feat: AcademicsEntity time 칼럼 삭제

--- src/main/kotlin/com/wafflestudio/csereal/core/academics/database/AcademicsSearchEntity.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/academics/database/AcademicsSearchEntity.kt
@@ -47,7 +47,6 @@ class AcademicsSearchEntity(
         fun createContent(academics: AcademicsEntity): String {
             val sb = StringBuilder()
             academics.name.let { sb.appendLine(it) }
-            academics.time?.let { sb.appendLine(it) }
             academics.year?.let { sb.appendLine(it) }
             sb.appendLine(academics.studentType.value)
             sb.appendLine(
