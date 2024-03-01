de0e357f0198b78b4a3591723e3c2e0b92196aa9
huGgW <whjoon0225@naver.com>
2024-03-01 13:32:35 +0900

    Feat: Change res body dir, merge two seperate body into one, add language field

--- src/main/kotlin/com/wafflestudio/csereal/core/member/api/res/MemberSearchResBody.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/member/api/res/MemberSearchResBody.kt
@@ -1,9 +1,9 @@
-package com.wafflestudio.csereal.core.member.dto
+package com.wafflestudio.csereal.core.member.api.res
 
 import com.wafflestudio.csereal.core.member.database.MemberSearchEntity
 import com.wafflestudio.csereal.core.resource.mainImage.database.MainImageEntity
 
-data class MemberSearchPageResponse(
+data class MemberSearchResBody(
     val members: List<MemberSearchResponseElement>,
     val total: Long
 ) {
@@ -12,7 +12,7 @@ data class MemberSearchPageResponse(
             members: List<MemberSearchEntity>,
             total: Long,
             imageURLMaker: (MainImageEntity?) -> String?
-        ) = MemberSearchPageResponse(
+        ) = MemberSearchResBody(
             members = members.map { MemberSearchResponseElement.of(it, imageURLMaker) },
             total = total
         )
