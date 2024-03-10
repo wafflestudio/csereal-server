38a3f9328ba1988ea426d17789024c5895ca62b4
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2024-03-10 20:16:38 +0900

    fix: readDegreeRequirements에 파일 보이도록 추가

--- src/main/kotlin/com/wafflestudio/csereal/core/academics/dto/DegreeRequirementsDto.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/academics/dto/DegreeRequirementsDto.kt
@@ -1,16 +1,20 @@
 package com.wafflestudio.csereal.core.academics.dto
 
 import com.wafflestudio.csereal.core.academics.database.AcademicsEntity
+import com.wafflestudio.csereal.core.resource.attachment.dto.AttachmentResponse
 
 class DegreeRequirementsDto(
     val year: Int,
-    val description: String
+    val description: String,
+    val attachments: List<AttachmentResponse>
+
 ) {
     companion object {
-        fun of(entity: AcademicsEntity) = entity.run {
+        fun of(entity: AcademicsEntity, attachments: List<AttachmentResponse>) = entity.run {
             DegreeRequirementsDto(
                 year = this.year!!,
-                description = this.description
+                description = this.description,
+                attachments = attachments
             )
         }
     }
