eb8de50462537c7aadd35a129b0078a3e5a024e6
leeeryboy <indiv0227@snu.ac.kr>
2024-04-04 16:12:39 +0900

    feat: delete mock login api in prod

--- src/main/kotlin/com/wafflestudio/csereal/common/mockauth/DevAuthController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/mockauth/DevAuthController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Profile("dev")
+@Profile("!prod")
 @RestController
 @RequestMapping("/api/v1")
 class DevAuthController(
