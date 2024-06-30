ffba3b6171785742c156ed1d302c9bfa3f89d5e7
huGgW <whjoon0225@naver.com>
2024-06-30 16:34:39 +0900

    Refactor: Remove unused interface

--- src/main/kotlin/com/wafflestudio/csereal/common/controller/ContentEntityType.kt
+++
@@ -1,7 +0,0 @@
-package com.wafflestudio.csereal.common.controller
-
-import com.wafflestudio.csereal.core.resource.mainImage.database.MainImageEntity
-
-interface ContentEntityType {
-    fun bringMainImage(): MainImageEntity?
-}
