fca6a2042cce4d9f01a7dc327078a6b02a171f09
huGgW <whjoon0225@naver.com>
2024-06-30 16:35:14 +0900

    Feat: Create Internal Entity, Dto

---
+++ src/main/kotlin/com/wafflestudio/csereal/core/internal/database/InternalEntity.kt
@@ -0,0 +1,22 @@
+package com.wafflestudio.csereal.core.internal.database
+
+import com.wafflestudio.csereal.common.config.BaseTimeEntity
+import com.wafflestudio.csereal.core.internal.dto.InternalDto
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.validation.constraints.NotBlank
+
+@Entity(name = "internal")
+class InternalEntity(
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
+    var description: String
+) : BaseTimeEntity() {
+    fun update(dto: InternalDto) {
+        description = dto.description
+    }
+
+    companion object {
+        fun of(dto: InternalDto) = InternalEntity(dto.description)
+    }
+}
