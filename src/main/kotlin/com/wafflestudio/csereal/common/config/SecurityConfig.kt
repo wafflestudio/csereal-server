8346d45732d2b882b3f8be28c653f94a65a1f680
leeeryboy <indiv0227@snu.ac.kr>
2024-07-14 23:28:08 +0900

    remove unnecessary import

--- src/main/kotlin/com/wafflestudio/csereal/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/config/SecurityConfig.kt
@@ -15,7 +15,6 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
