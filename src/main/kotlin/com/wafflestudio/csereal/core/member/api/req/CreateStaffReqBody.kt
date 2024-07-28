407a76579fd09e4f4df9a365b7fac7ad040527ab
huGgW <whjoon0225@naver.com>
2024-07-28 20:38:03 +0900

    refactor: remove language field from single req body

--- src/main/kotlin/com/wafflestudio/csereal/core/member/api/req/CreateStaffReqBody.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/member/api/req/CreateStaffReqBody.kt
@@ -1,7 +1,6 @@
 package com.wafflestudio.csereal.core.member.api.req
 
 data class CreateStaffReqBody(
-    val language: String,
     val name: String,
     val role: String,
     val office: String,
