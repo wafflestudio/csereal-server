02b9d98cfc3e2e0b3ae1af8f05d0d8d317b68506
huGgW <whjoon0225@naver.com>
2024-09-17 18:21:01 +0900

    feat: add LAB CRUD APIs

--- src/main/kotlin/com/wafflestudio/csereal/core/research/api/v1/ResearchController.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/research/api/v1/ResearchController.kt
@@ -1,25 +1,25 @@
 package com.wafflestudio.csereal.core.research.api.v1
 
-import com.wafflestudio.csereal.common.aop.AuthenticatedStaff
 import com.wafflestudio.csereal.common.enums.LanguageType
 import com.wafflestudio.csereal.core.research.dto.LabDto
-import com.wafflestudio.csereal.core.research.dto.LabUpdateRequest
 import com.wafflestudio.csereal.core.research.dto.ResearchDto
 import com.wafflestudio.csereal.core.research.dto.ResearchGroupResponse
+import com.wafflestudio.csereal.core.research.service.LabService
+import com.wafflestudio.csereal.core.research.service.LabServiceImpl
 import com.wafflestudio.csereal.core.research.service.ResearchSearchService
 import com.wafflestudio.csereal.core.research.service.ResearchService
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Positive
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
 
 @RequestMapping("/api/v1/research")
 @RestController("ResearchControllerV1")
 @Deprecated(message = "Use V2 API")
 class ResearchController(
     private val researchService: ResearchService,
-    private val researchSearchService: ResearchSearchService
+    private val researchSearchService: ResearchSearchService,
+    private val labService: LabService,
 ) {
     @GetMapping("/groups")
     fun readAllResearchGroups(
@@ -35,45 +35,20 @@ class ResearchController(
         return ResponseEntity.ok(researchService.readAllResearchCentersDeprecated(language))
     }
 
-    @AuthenticatedStaff
-    @PostMapping("/lab")
-    fun createLab(
-        @Valid
-        @RequestPart("request")
-        request: LabDto,
-        @RequestPart("pdf") pdf: MultipartFile?
-    ): ResponseEntity<LabDto> {
-        return ResponseEntity.ok(researchService.createLab(request, pdf))
-    }
-
     @GetMapping("/labs")
     fun readAllLabs(
         @RequestParam(required = false, defaultValue = "ko") language: String
     ): ResponseEntity<List<LabDto>> {
-        return ResponseEntity.ok(researchService.readAllLabs(language))
+        return ResponseEntity.ok(labService.readAllLabs(language))
     }
 
     @GetMapping("/lab/{labId}")
     fun readLab(
         @PathVariable labId: Long
     ): ResponseEntity<LabDto> {
-        return ResponseEntity.ok(researchService.readLab(labId))
+        return ResponseEntity.ok(labService.readLab(labId))
     }
 
-    /**
-     * Research Group 수정은 일단 제외하였음.
-     */
-    @AuthenticatedStaff
-    @PatchMapping("/lab/{labId}")
-    fun updateLab(
-        @PathVariable labId: Long,
-        @Valid
-        @RequestPart("request")
-        request: LabUpdateRequest,
-        @RequestPart("pdf") pdf: MultipartFile?
-    ): ResponseEntity<LabDto> {
-        return ResponseEntity.ok(researchService.updateLab(labId, request, pdf))
-    }
 
     @GetMapping("/search/top")
     fun searchTop(
