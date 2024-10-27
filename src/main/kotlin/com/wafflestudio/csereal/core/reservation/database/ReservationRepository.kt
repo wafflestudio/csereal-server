0cc2119c2401e0085b103769797a223f2d0d6a91
leeeryboy <indiv0227@snu.ac.kr>
2024-10-27 16:22:46 +0900

    remove unnecessary import

--- src/main/kotlin/com/wafflestudio/csereal/core/reservation/database/ReservationRepository.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/reservation/database/ReservationRepository.kt
@@ -1,8 +1,6 @@
 package com.wafflestudio.csereal.core.reservation.database
 
-import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 import java.time.LocalDateTime
 import java.util.UUID
