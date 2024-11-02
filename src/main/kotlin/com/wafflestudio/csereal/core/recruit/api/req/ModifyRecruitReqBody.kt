9c332dea727c62151c181f6707f74f87d45907d8
logan <logan@channel.io>
2024-11-03 00:01:54 +0900

    Feat: Add ModifyRecruitReqBody

---
+++ src/main/kotlin/com/wafflestudio/csereal/core/recruit/api/req/ModifyRecruitReqBody.kt
@@ -0,0 +1,8 @@
+package com.wafflestudio.csereal.core.recruit.api.req
+
+data class ModifyRecruitReqBody(
+    val latestRecruitTitle: String,
+    val latestRecruitUrl: String,
+    val description: String,
+    val removeImage: Boolean,
+)
