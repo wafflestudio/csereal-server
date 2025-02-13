b69fb8d76e8d0479fe30674983abc506a8829e60
Junhyeong Kim <leeeryboy@gmail.com>
2025-02-14 01:40:42 +0900

    change type to enum

--- src/main/resources/db/migration/V3__update_council.sql
+++ src/main/resources/db/migration/V3__update_council.sql
@@ -1,10 +1,10 @@
 ALTER TABLE council
-    ADD COLUMN type          VARCHAR(50)  NOT NULL,
-    ADD COLUMN title         VARCHAR(255) NOT NULL,
-    ADD COLUMN description   MEDIUMTEXT   NOT NULL,
-    ADD COLUMN main_image_id BIGINT       NULL,
-    ADD COLUMN sequence      INT          NOT NULL,
-    ADD COLUMN name          VARCHAR(255) NOT NULL;
+    ADD COLUMN type          ENUM('INTRO', 'REPORT') NOT NULL,
+    ADD COLUMN title         VARCHAR(255)            NOT NULL,
+    ADD COLUMN description   MEDIUMTEXT              NOT NULL,
+    ADD COLUMN main_image_id BIGINT                  NULL,
+    ADD COLUMN sequence      INT                     NOT NULL,
+    ADD COLUMN name          VARCHAR(255)            NOT NULL;
 
 ALTER TABLE council
     ADD CONSTRAINT fk_council_main_image_main_image_id
