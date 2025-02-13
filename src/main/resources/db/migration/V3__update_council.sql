27b677e184437af8a59194c9a1cd2e851c8678da
leeeryboy <indiv0227@snu.ac.kr>
2025-02-13 16:18:35 +0900

    sql for updated council entity

---
+++ src/main/resources/db/migration/V3__update_council.sql
@@ -0,0 +1,11 @@
+ALTER TABLE council
+    ADD COLUMN type          VARCHAR(50)  NOT NULL,
+    ADD COLUMN title         VARCHAR(255) NOT NULL,
+    ADD COLUMN description   MEDIUMTEXT   NOT NULL,
+    ADD COLUMN main_image_id BIGINT       NULL,
+    ADD COLUMN sequence      INT          NOT NULL,
+    ADD COLUMN name          VARCHAR(255) NOT NULL;
+
+ALTER TABLE council
+    ADD CONSTRAINT fk_council_main_image_main_image_id
+        FOREIGN KEY (main_image_id) REFERENCES main_image (id);
