98155a2c2f3adfedb2663b686e77d0cafdcc4f86
ChungPlusPlus <jjy051221@gmail.com>
2025-04-05 10:53:51 +0900

    feat: V8_update_members

---
+++ src/main/resources/db/migration/V8__update_members.sql
@@ -0,0 +1,38 @@
+ALTER TABLE professor
+    ADD COLUMN educations       TEXT    NULL,
+    ADD COLUMN research_areas   TEXT    NULL;
+ALTER TABLE staff
+    ADD COLUMN tasks            TEXT    NULL;
+
+UPDATE professor
+SET educations = (
+    SELECT JSON_ARRAYAGG(education.name)
+    FROM education
+    WHERE education.professor_id = professor.id
+    ORDER BY education.id
+)
+WHERE EXISTS (
+    SELECT * FROM education WHERE education.professor_id = professor.id
+);
+
+UPDATE professor
+SET research_areas = (
+    SELECT JSON_ARRAYAGG(research_area.name)
+    FROM research_area
+    WHERE research_area.professor_id = professor.id
+    ORDER BY research_area.id
+)
+WHERE EXISTS (
+    SELECT * FROM research_area WHERE research_area.professor_id = professor.id
+);
+
+UPDATE staff
+SET tasks = (
+    SELECT JSON_ARRAYAGG(task.name)
+    FROM task
+    WHERE task.staff_id = staff.id
+    ORDER BY task.id
+)
+WHERE EXISTS (
+    SELECT * FROM task WHERE task.staff_id = staff.id
+);
