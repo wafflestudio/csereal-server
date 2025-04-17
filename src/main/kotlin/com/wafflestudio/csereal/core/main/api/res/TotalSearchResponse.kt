a9fa2c79ae4968c22459c8a0c137ee27b3c2de65
ChungPlusPlus <jjy051221@gmail.com>
2025-04-18 00:15:33 +0900

    feat: totalSearchApi

---
+++ src/main/kotlin/com/wafflestudio/csereal/core/main/api/res/TotalSearchResponse.kt
@@ -0,0 +1,21 @@
+package com.wafflestudio.csereal.core.main.api.res
+
+import com.wafflestudio.csereal.core.about.api.res.AboutSearchResBody
+import com.wafflestudio.csereal.core.academics.api.res.AcademicsSearchResBody
+import com.wafflestudio.csereal.core.admissions.api.res.AdmissionSearchResBody
+import com.wafflestudio.csereal.core.member.api.res.MemberSearchResBody
+import com.wafflestudio.csereal.core.news.dto.NewsTotalSearchDto
+import com.wafflestudio.csereal.core.notice.dto.NoticeTotalSearchResponse
+import com.wafflestudio.csereal.core.research.api.res.ResearchSearchResBody
+import com.wafflestudio.csereal.core.seminar.dto.SeminarSearchResponse
+
+data class TotalSearchResponse (
+    val aboutResult: AboutSearchResBody,
+    val noticeResult: NoticeTotalSearchResponse,
+    val newsResult: NewsTotalSearchDto,
+    val seminarResult: SeminarSearchResponse,
+    val memberResult: MemberSearchResBody,
+    val researchResult: ResearchSearchResBody,
+    val admissionsResult: AdmissionSearchResBody,
+    val academicsResult: AcademicsSearchResBody,
+)
--- src/main/kotlin/com/wafflestudio/csereal/core/main/service/MainService.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/main/service/MainService.kt
@@ -1,15 +1,27 @@
 package com.wafflestudio.csereal.core.main.service
 
+import com.wafflestudio.csereal.common.enums.ContentSearchSortType
+import com.wafflestudio.csereal.common.enums.LanguageType
+import com.wafflestudio.csereal.core.about.service.AboutService
+import com.wafflestudio.csereal.core.academics.service.AcademicsSearchService
+import com.wafflestudio.csereal.core.admissions.service.AdmissionsService
+import com.wafflestudio.csereal.core.main.api.res.TotalSearchResponse
 import com.wafflestudio.csereal.core.main.database.MainRepository
 import com.wafflestudio.csereal.core.main.dto.MainImportantResponse
 import com.wafflestudio.csereal.core.main.dto.MainResponse
 import com.wafflestudio.csereal.core.main.dto.NoticesResponse
 import com.wafflestudio.csereal.core.main.event.RefreshSearchEvent
+import com.wafflestudio.csereal.core.member.service.MemberSearchService
 import com.wafflestudio.csereal.core.news.database.NewsRepository
+import com.wafflestudio.csereal.core.news.service.NewsService
 import com.wafflestudio.csereal.core.notice.database.NoticeRepository
 import com.wafflestudio.csereal.core.notice.database.TagInNoticeEnum
+import com.wafflestudio.csereal.core.notice.service.NoticeService
+import com.wafflestudio.csereal.core.research.service.ResearchSearchService
 import com.wafflestudio.csereal.core.seminar.database.SeminarRepository
+import com.wafflestudio.csereal.core.seminar.service.SeminarService
 import org.springframework.context.ApplicationEventPublisher
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -17,6 +29,13 @@ interface MainService {
     fun readMain(importantCnt: Int?): MainResponse
     fun refreshSearch()
     fun readMainImportant(cnt: Int? = null): List<MainImportantResponse>
+    fun totalSearch(
+        keyword: String,
+        number: Int,
+        memberNumber: Int,
+        stringLength: Int,
+        language: LanguageType
+    ): TotalSearchResponse
 }
 
 @Service
@@ -25,7 +44,15 @@ class MainServiceImpl(
     private val eventPublisher: ApplicationEventPublisher,
     private val noticeRepository: NoticeRepository,
     private val seminarRepository: SeminarRepository,
-    private val newsRepository: NewsRepository
+    private val newsRepository: NewsRepository,
+    private val aboutService: AboutService,
+    private val noticeService: NoticeService,
+    private val newsService: NewsService,
+    private val seminarService: SeminarService,
+    private val memberSearchService: MemberSearchService,
+    private val researchSearchService: ResearchSearchService,
+    private val admissionsService: AdmissionsService,
+    private val academicsSearchService: AcademicsSearchService,
 ) : MainService {
     @Transactional(readOnly = true)
     override fun readMain(importantCnt: Int?): MainResponse {
@@ -55,5 +82,71 @@ class MainServiceImpl(
 
     override fun refreshSearch() {
         eventPublisher.publishEvent(RefreshSearchEvent())
+    }
+
+    @Transactional(readOnly = true)
+    override fun totalSearch(
+        keyword: String,
+        number: Int,
+        memberNumber: Int,
+        stringLength: Int,
+        language: LanguageType,
+    ): TotalSearchResponse {
+        val aboutResult = aboutService.searchTopAbout(
+            keyword,
+            language,
+            number,
+            stringLength,
+        )
+        val noticeResult = noticeService.searchTotalNotice(
+            keyword,
+            number,
+            stringLength
+        )
+        val newsResult = newsService.searchTotalNews(
+            keyword,
+            number,
+            stringLength
+        )
+        val seminarResult = seminarService.searchSeminar(
+            keyword,
+            PageRequest.of(0, 10),
+            false,
+            ContentSearchSortType.DATE
+        )
+        val memberResult = memberSearchService.searchTopMember(
+            keyword,
+            language,
+            memberNumber
+        )
+        val researchResult = researchSearchService.searchTopResearch(
+            keyword,
+            language,
+            number,
+            stringLength
+        )
+        val admissionsResult = admissionsService.searchTopAdmission(
+            keyword,
+            language,
+            number,
+            stringLength
+        )
+        val academicsResult = academicsSearchService.searchTopAcademics(
+            keyword,
+            language,
+            number,
+            stringLength
+        )
+
+        return TotalSearchResponse(
+            aboutResult = aboutResult,
+            noticeResult = noticeResult,
+            newsResult = newsResult,
+            seminarResult = seminarResult,
+            memberResult = memberResult,
+            researchResult = researchResult,
+            admissionsResult = admissionsResult,
+            academicsResult = academicsResult,
+        )
     }
 }
