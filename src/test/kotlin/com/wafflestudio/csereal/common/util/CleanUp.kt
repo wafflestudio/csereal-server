615fa2ac5ef04b1862975a243b687ebb03d446dc
ChungPlusPlus <jjy051221@gmail.com>
2025-05-15 16:24:00 +0900

    fix: use guava to change CaseFormat

--- src/test/kotlin/com/wafflestudio/csereal/common/util/CleanUp.kt
+++ src/test/kotlin/com/wafflestudio/csereal/common/util/CleanUp.kt
@@ -6,7 +6,7 @@ import jakarta.transaction.Transactional
 import org.springframework.context.annotation.Profile
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
-import java.util.*
+import com.google.common.base.CaseFormat
 
 @Profile("test")
 @Component
@@ -14,11 +14,6 @@ class CleanUp(
     private var jdbcTemplate: JdbcTemplate,
     private var entityManager: EntityManager
 ) {
-    private fun camelToSnake(camelCase: String): String {
-        return camelCase.replace(Regex("([a-z])([A-Z])"), "$1_$2")
-            .lowercase(Locale.getDefault())
-    }
-
     @Transactional
     fun all() {
         val tables = entityManager.metamodel.entities.mapNotNull { entityType ->
@@ -33,7 +28,8 @@ class CleanUp(
         jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0")
 
         tables.forEach { table ->
-            val tableNameInSnakeCase = camelToSnake(table) // 테이블 이름을 Snake_case로 변환
+            val tableNameInSnakeCase = CaseFormat.LOWER_CAMEL
+                .to(CaseFormat.LOWER_UNDERSCORE, table) // 테이블 이름을 Snake Case로 변환
             try {
                 jdbcTemplate.execute("TRUNCATE TABLE `$tableNameInSnakeCase`") // 변환된 이름 사용
             } catch (e: Exception) {
