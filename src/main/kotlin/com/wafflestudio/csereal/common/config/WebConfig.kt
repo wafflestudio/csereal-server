5e139037c9b28aa7504a2c0959fd7aa22212c2f3
huGgW <whjoon0225@naver.com>
2025-08-04 01:53:58 +0900

    feat: add client info interceptor to webconfig

--- src/main/kotlin/com/wafflestudio/csereal/common/config/WebConfig.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/config/WebConfig.kt
@@ -1,15 +1,18 @@
 package com.wafflestudio.csereal.common.config
 
+import com.wafflestudio.csereal.common.interceptor.ClientInfoInterceptor
 import com.wafflestudio.csereal.common.properties.EndpointProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableConfigurationProperties(EndpointProperties::class)
 class WebConfig(
-    private val endpointProperties: EndpointProperties
+    private val endpointProperties: EndpointProperties,
+    private val clientInfoInterceptor: ClientInfoInterceptor
 ) : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
@@ -19,5 +22,9 @@ class WebConfig(
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3000)
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(clientInfoInterceptor)
     }
 }
