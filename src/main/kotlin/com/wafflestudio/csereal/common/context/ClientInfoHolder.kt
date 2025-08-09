9f0607098cc34060054190cdd35c5b0f9319cf3a
huGgW <whjoon0225@naver.com>
2025-08-10 01:15:25 +0900

    feat: seperate info context inteface

--- src/main/kotlin/com/wafflestudio/csereal/common/context/ClientInfoHolder.kt
+++ src/main/kotlin/com/wafflestudio/csereal/common/context/ClientInfoHolder.kt
@@ -4,9 +4,14 @@ import com.wafflestudio.csereal.common.dto.ClientInfo
 import org.springframework.stereotype.Component
 import org.springframework.web.context.annotation.RequestScope
 
+interface ClientInfoContext {
+    fun getClientInfo(): ClientInfo
+}
+
 @Component
 @RequestScope
-class ClientInfoHolder {
+class ClientInfoHolder : ClientInfoContext {
     lateinit var clientInfo: ClientInfo
-        internal set
+
+    override fun getClientInfo() = clientInfo
 }
