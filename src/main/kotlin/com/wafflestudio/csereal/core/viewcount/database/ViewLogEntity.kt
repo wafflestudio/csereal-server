7ed0b00d19893e578e586ea602e7b49b81ee6fdd
huGgW <whjoon0225@naver.com>
2025-08-17 17:53:14 +0900

    TMP

--- src/main/kotlin/com/wafflestudio/csereal/core/viewcount/database/ViewLogEntity.kt
+++ src/main/kotlin/com/wafflestudio/csereal/core/viewcount/database/ViewLogEntity.kt
@@ -6,9 +6,18 @@ import com.wafflestudio.csereal.common.entity.BaseTimeEntity
 import jakarta.persistence.*
 
 @Entity(name = "view_log")
+@Table(
+    indexes = [
+        Index(
+            name = "IDX_view_log_domain_domain_id_created_at",
+            columnList = "domain, domain_id, created_at"
+        )
+    ]
+)
 class ViewLogEntity(
     @Enumerated(value = EnumType.STRING)
     var domain: Domain,
     var domainId: Long,
+    @Convert
     var clientInfo: ClientInfo,
 ) : BaseTimeEntity()
