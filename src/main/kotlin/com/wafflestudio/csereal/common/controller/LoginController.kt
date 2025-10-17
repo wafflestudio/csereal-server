e667c1cd8eb47c8dd695f9fdcb939a74c2cc19b8
ChungPlusPlus <jjy051221@gmail.com>
2025-10-17 14:45:57 +0900

    add api/v2/login to allways  allow login

---
+++ src/main/kotlin/com/wafflestudio/csereal/common/controller/LoginController.kt
@@ -0,0 +1,20 @@
+package com.wafflestudio.csereal.common.controller
+
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Profile("!test")
+@RestController
+class LoginController(
+    @Value("\${login-page}")
+    private val loginPage: String
+) {
+    @GetMapping("/api/v2/login")
+    fun redirectToLoginURL(response: HttpServletResponse) {
+        val redirectUrl = "$loginPage/oauth2/authorization/idsnucse"
+        response.sendRedirect(redirectUrl)
+    }
+}
