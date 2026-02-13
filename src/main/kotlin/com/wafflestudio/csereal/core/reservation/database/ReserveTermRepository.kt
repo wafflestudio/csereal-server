7f975321841cd926a1c94cac6c88b50e9802c296
ChungPlusPlus <jjy051221@gmail.com>
2026-02-13 19:21:17 +0900

    added ReserveTerm Entity

---
+++ src/main/kotlin/com/wafflestudio/csereal/core/reservation/database/ReserveTermRepository.kt
@@ -0,0 +1,30 @@
+package com.wafflestudio.csereal.core.reservation.database
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.LocalDateTime
+
+interface ReserveTermRepository : JpaRepository<ReserveTermEntity, Long> {
+
+    @Query("""
+        SELECT rt FROM reserve_term rt
+        WHERE rt.termStartTime <= :start AND rt.termEndTime >= :end
+        """
+    )
+    fun findFirstByTimeInclude(
+        @Param("start") start: LocalDateTime, @Param("end") end: LocalDateTime
+    ): List<ReserveTermEntity>
+
+    @Query("""
+        SELECT rt FROM reserve_term rt 
+        WHERE rt.termStartTime < :end AND rt.termEndTime > :start
+        """
+    )
+    fun findByTimeOverlap(
+        @Param("start") start: LocalDateTime, @Param("end") end: LocalDateTime
+    ): List<ReserveTermEntity>
+
+    @Query("""SELECT MAX(rt.termEndTime) FROM reserve_term rt""")
+    fun findLastEndTime(): LocalDateTime?
+}
